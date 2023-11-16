9f8586fc7ace94c911248956b48fa9e837ff30eb
JarnotMaciej <mj300741@student.polsl.pl>
2023-11-16 18:24:30 +0100

    tabbed pane, icons & stuff

--- mj/src/main/java/pl/polsl/mj/Main.java
+++ mj/src/main/java/pl/polsl/mj/Main.java
@@ -1,7 +1,10 @@
 package pl.polsl.mj;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+
 import pl.polsl.mj.view.*;
+
+import static javax.swing.UIManager.*;
 
 /**
  * Main class, responsible for running the program.
@@ -18,11 +21,12 @@ public class Main {
     private static void createAndShowGUI() {        
         // JFrame.setDefaultLookAndFeelDecorated(true);
 
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("Arabic-Roman Converter");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         View newContentPane = new View();
         frame.setContentPane(newContentPane);
+        frame.setIconImage(newContentPane.getAppIcon().getImage());
 
         frame.pack();
         frame.setVisible(true);
@@ -33,6 +37,12 @@ public class Main {
  * @param args the command line arguments -> not used
 */
     public static void main(String[] args) {
+        try {
+            setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+//            setLookAndFeel(getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -42,3 +52,10 @@ public class Main {
         });
     }
 }
+
+// TODO:
+//- GUI application based on Swing, including:
+//        - [x] an advanced container (tabbed pane, split pane),
+//        - [x] message box (JOptionPane for Swing) to indicate validation errors or information messages,
+//        - [ ] table to store the history of performed operations,
+//        - [ ] menu
--- mj/src/main/java/pl/polsl/mj/view/View.java
+++ mj/src/main/java/pl/polsl/mj/view/View.java
@@ -1,17 +1,12 @@
 package pl.polsl.mj.view;
 
+import pl.polsl.mj.model.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.util.stream.IntStream;
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 /**
  * View class, responsible for displaying GUI to the user.
@@ -19,60 +14,152 @@ import javax.swing.JTextField;
  * @author mj300741@student.polsl.pl
  * @version 1.1
  */
-public class View extends JFrame implements ActionListener {
+public class View extends JPanel implements ActionListener {
+    /**
+     * Model object.
+     */
+    Model model = new Model();
 
-/**
- * Constructor of the View class.
- */
-public View() {
-        super("Arabic-Roman converter");
+    /**
+     * App icon.
+     */
+    ImageIcon appIcon;
 
-        JLabel label = new JLabel("Enter number to convert: ");
-        JTextField textField = new JTextField();
-        JRadioButton radioButton1 = new JRadioButton("Arabic to Roman");
-        JRadioButton radioButton2 = new JRadioButton("Roman to Arabic");
-        JButton confirmBtn = new JButton("Convert");
-        JLabel resultLabel = new JLabel("Result will be displayed here");
-
-        confirmBtn.setActionCommand("convert");
-        radioButton1.setActionCommand("arabicToRoman");
-        radioButton2.setActionCommand("romanToArabic");
-
-        // listeners
-        confirmBtn.addActionListener(this);
-        radioButton1.addActionListener(this);
-        radioButton2.addActionListener(this);
-
-        ButtonGroup buttonGroup = new ButtonGroup();
-        buttonGroup.add(radioButton1);
-        buttonGroup.add(radioButton2);
-
-        setLayout(null);
-
-        // Component placement
-        label.setBounds(10, 10, 200, 30);
-        textField.setBounds(10, 50, 200, 30);
-        radioButton1.setBounds(10, 90, 150, 30);
-        radioButton2.setBounds(10, 130, 150, 30);
-        confirmBtn.setBounds(10, 170, 100, 30);
-        resultLabel.setBounds(10, 210, 250, 30);
-
-        // Add components to the frame
-        add(label);
-        add(textField);
-        add(radioButton1);
-        add(radioButton2);
-        add(confirmBtn);
-        add(resultLabel);
-
-        setSize(600, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+    /**
+     * Getter for app icon.
+     * @return app icon
+     */
+    public ImageIcon getAppIcon() {
+        return appIcon;
     }
 
-/**
- * Overriding actionPerformed method from ActionListener interface.
- */
+    /**
+     * Constructor of the View class.
+     */
+    public View() {
+        super(new GridLayout(1, 1));
+        JTabbedPane tabbedPane = new JTabbedPane();
+        ImageIcon arabic = createImageIcon("/images/arabic.png");
+        ImageIcon roman = createImageIcon("/images/roman.png");
+        appIcon = createImageIcon("/images/app.png");
+
+        JComponent arabicPanel = makeArabicPanel();
+        tabbedPane.addTab("Arabic", arabic, arabicPanel,
+                "Arabic to Roman");
+        tabbedPane.setMnemonicAt(0, 1);
+
+        JComponent romanPanel = makeRomanPanel();
+        tabbedPane.addTab("Roman", roman, romanPanel,
+                "Roman to Arabic");
+        tabbedPane.setMnemonicAt(1, 2);
+
+        add(tabbedPane);
+    }
+
+    /**
+     * Method responsible for creating panel with Roman to Arabic converter.
+     * @return JComponent object with Roman to Arabic converter
+     */
+    private JComponent makeRomanPanel() {
+        JPanel panel = new JPanel(false);
+        JLabel label = new JLabel("Enter Roman numeral:");
+        JTextField textField = new JTextField(10);
+        JButton button = new JButton("Convert");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String input = textField.getText();
+
+                // Validate the input as Roman numeral
+                if (model.validateRoman(input)) {
+                    try {
+                        // Convert Roman to Arabic using the model method
+                        int arabicNumber = model.romanToArabic(input);
+
+                        // Display the result or perform any desired action
+                        JOptionPane.showMessageDialog(null, "Arabic number: " + arabicNumber, "Conversion Result", JOptionPane.INFORMATION_MESSAGE);
+                    } catch (ModelException ex) {
+                        // Handle exception, e.g., invalid input
+                        JOptionPane.showMessageDialog(null, "Invalid Roman numeral", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                } else {
+                    // Handle invalid input
+                    JOptionPane.showMessageDialog(null, "Invalid Roman numeral", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        panel.add(label);
+        panel.add(textField);
+        panel.add(button);
+        return panel;
+    }
+
+    /**
+     * Method responsible for creating panel with Arabic to Roman converter.
+     * @return JComponent object with Arabic to Roman converter
+     */
+    private JComponent makeArabicPanel() {
+        JPanel panel = new JPanel(false);
+        JLabel label = new JLabel("Enter Arabic number:");
+        JTextField textField = new JTextField(10);
+        JButton button = new JButton("Convert");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String input = textField.getText();
+
+                // Validate the input as Arabic number
+                if (model.validateArabic(input)) {
+                    try {
+                        int arabicNumber = Integer.parseInt(input);
+
+                        // Convert Arabic to Roman using the model method
+                        String romanNumeral = model.arabicToRoman(arabicNumber);
+
+                        // Display the result or perform any desired action
+                        JOptionPane.showMessageDialog(null, "Roman Numeral: " + romanNumeral, "Conversion Result", JOptionPane.INFORMATION_MESSAGE);
+                    } catch (NumberFormatException | ModelException ex) {
+                        // Handle exception, e.g., invalid input
+                        JOptionPane.showMessageDialog(null, "Invalid Arabic number", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                } else {
+                    // Handle invalid input
+                    JOptionPane.showMessageDialog(null, "Invalid Arabic number", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        panel.add(label);
+        panel.add(textField);
+        panel.add(button);
+        return panel;
+    }
+
+    /**
+     * Method responsible for loading image from file.
+     * @param path path to the image
+     * @return ImageIcon object
+     */
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = View.class.getResource(path);
+        if (imgURL != null) {
+            ImageIcon icon = new ImageIcon(imgURL);
+            Image image = icon.getImage();
+            Image newimg = image.getScaledInstance(30, 30, Image.SCALE_AREA_AVERAGING);
+            icon = new ImageIcon(newimg);
+
+            return icon;
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+
+    /**
+     * Overriding actionPerformed method from ActionListener interface.
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         // Handle button click event
@@ -81,6 +168,7 @@ public View() {
 
     /**
      * Getting input from user
+     *
      * @return input from user
      */
     public String getInput() {
